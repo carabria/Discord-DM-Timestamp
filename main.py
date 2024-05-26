949557f877c3a06f819de797f1b9d40cf7674aac
carabria <studyingrhystic@gmail.com>
2024-05-26 01:49:25 -0400

    logging and main file set up

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
+import logging
 import discord
+import sys
 
-intents = discord.Intents.default()
-intents.message_content = True
+class Main:
+    def __init__(self):
+        #init client
+        intents = discord.Intents.default()
+        intents.message_content = True
+        self.client = discord.Client(intents=intents)
 
-client = discord.Client(intents=intents)
+        #run function to confirm bot is logged in and working properly 
+        self.on_startup()
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+        #run client
+        self.login()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    def on_startup(self):
+        @self.client.event
+        async def on_ready():
+            print(f'Logged in as {self.client.user}')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        @self.client.event
+        async def on_message(message):
+            if message.author == self.client.user:
+                return
 
-with open('token.txt', 'r') as file:
-    token = file.read().strip()
+            if message.content.startswith('$hello'):
+                await message.channel.send('Hello!')
 
-client.run(token)
+    def login(self):
+        #init logger
+        handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
+
+        #read token from token.txt
+        try:
+            with open('token.txt', 'r') as file:
+                token = file.read().strip()
+                self.client.run(token, log_handler=handler, log_level=logging.DEBUG)
+        except FileNotFoundError:
+            print("Please create a token.txt file for the program to run!")
+            sys.exit()
+
+if __name__ == "__main__":
+    main_instance = Main()
+    main_instance.login()
