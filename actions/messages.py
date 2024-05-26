ea5b6b27123ab943af61fceed2626f48d57f8712
carabria <studyingrhystic@gmail.com>
2024-05-26 12:58:11 -0400

    short

--- actions/messages.py
+++ actions/messages.py
@@ -15,24 +15,24 @@ class Messages:
         if message.content.startswith('!hello'):
             await message.channel.send('world!')
 
-        if message.content.startswith("!time"):
+        if message.content.startswith("!time") | message.content.startswith("!t"):
             try:
                 #get current time as a unix timestamp
-                if message.content.endswith("help"):
+                if message.content.endswith("-h") | message.content.endswith("-help"):
                     await message.channel.send(
-                        'current: returns the current time!\nfrom now: returns x hours/days/minutes/etc from now!\nago: returns x hours/days/minutes/etc ago!')
+                        '-c (current): returns the current time!\n-fn (from now): returns x hours/days/minutes/etc from now!\n-a (ago): returns x hours/days/minutes/etc ago!')
                 
-                elif message.content.endswith("current"):
+                elif message.content.endswith("-c") | message.content.endswith("-current"):
                     await message.channel.send(f'The current time is <t:{timers.Actions.current_time()}>!')
                 
-                elif message.content.endswith("from now"):
+                elif message.content.endswith("-fn") | message.content.endswith("-from now"):
                     await message.channel.send(f'That would be <t:{timers.Actions.time_from_now(message.content)}>')
 
-                elif message.content.endswith("ago"):
+                elif message.content.endswith("-a") | message.content.endswith ("-ago"):
                     await message.channel.send(f'That would be <t:{timers.Actions.time_ago(message.content)}>!')
             
                 else:
-                    await message.channel.send('You don\'t seem to have inputted the command in correctly... Why nyot try using help?')
+                    await message.channel.send('You don\'t seem to have inputted the command in correctly... Use -h for help!')
             
             except custom_exceptions.NoTimeValueError as e:
                 await message.channel.send(f'{e}')
